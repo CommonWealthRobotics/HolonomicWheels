806aa89e3270baad10e015dc62e349ecfbaccd80
Kevin Harrington <kharrington@bancroftschool.org>
2024-05-20 10:19:51 -0400

    adding basic omniwheel

--- WheelFactory.groovy
+++ WheelFactory.groovy
@@ -2,16 +2,22 @@ import com.neuronrobotics.sdk.addons.kinematics.DHLink
 import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics
 import com.neuronrobotics.sdk.addons.kinematics.LinkConfiguration
 import com.neuronrobotics.sdk.addons.kinematics.MobileBase
+import com.neuronrobotics.sdk.addons.kinematics.VitaminLocation
+import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR
 
 import javafx.scene.transform.Affine
 if(args==null) {
-	args= new HashMap<String,Double>()
+	args= new HashMap<String,Object>()
 	args.put("diameter",101.6)
 	args.put("numRollers",13)
 	args.put("HubHeight",24)
 	args.put("rollerHeight", 12.5)
 	args.put("rollerDiameter",8)
 	args.put("numberOfRows",2)
+	args.put("electroMechanicalType","vexMotor")
+	args.put("electroMechanicalSize","v5_11w")
+	args.put("shaftType","vexWheels")
+	args.put("shaftSize","4InchOmni")
 }
 for(String key:args.keySet()) {
 	println "Key "+key+" value "+args.get(key)
@@ -25,6 +31,15 @@ DHParameterKinematics wheelLimb = new DHParameterKinematics();
 generated.getDrivable().add(wheelLimb);
 
 LinkConfiguration wheelConfig = new LinkConfiguration();
+wheelConfig.addVitamin(new VitaminLocation("electroMechanical", 
+											args.electroMechanicalType.toString(),
+											args.electroMechanicalSize.toString(),
+											  new TransformNR()))
+wheelConfig.addVitamin(new VitaminLocation("shaft",
+	args.shaftType.toString(),
+	args.shaftSize.toString(),
+	  new TransformNR()))
+	  
 DHLink dh = new DHLink(args.diameter/2, 0, 0, 0);
 dh.setListener(new Affine());
 wheelLimb.addNewLink(wheelConfig, dh)
